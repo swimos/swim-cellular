daa675eab65975da4f54ca8909b320df64baee60
ajay-gov <ajay.gov@protonmail.com>
2022-06-02 07:53:42 -0700

    Clean up to remove commented code

--- src/main/java/swim/cellular/broker/pulsar/SitePulsarAgent.java
+++ src/main/java/swim/cellular/broker/pulsar/SitePulsarAgent.java
@@ -53,31 +53,4 @@ public class SitePulsarAgent extends  PulsarAgent {
     return lastMessage;
   }
 
-  /*
-  @Override
-  protected void logMetrics() {
-    int i = 0;
-    while(i < 20000) {
-      final int category = Math.random() < 0.1 ? Math.random() < 0.1 ? 2 : 1 : 0;
-      // Add some fuzziness to the alert category to get a continuous severity
-      // value between 0 and 2.  A value of 0 represents no alert or warning.
-      // The interval (0, 1] represents a warning.  And the interval (1, 2]
-      // represents an alert.
-      final double severity = category == 0 ? 0.0 : (category - 1) + Math.random();
-
-      // Generate a random mean ul sinr value between 0 and 40.
-      final int meanUlSinr = (int) Math.round(Math.random() * 40);
-
-      // Generate a random rrc re-establishment failure count between 1 and 9.
-      final long rrcReestablishmentFailures = (long) (1 + Math.random() * 9);
-      final String jsonPayload = String.format(JSON_PATTERN, i, severity, meanUlSinr, rrcReestablishmentFailures);
-      makeCommand(jsonPayload);
-      i += 1;
-    }
-    super.logMetrics();
-  }
-
-  private static final String JSON_PATTERN = "{\"id\":%d, \"severity\":%f, \"mean_ul_sinr\":%d, \"rrc_re_establishment_failures\":%d}";
-  */
-
 }
